6e0170430eb2d1321a793f5f9b779c47a0821a3a
Igor Marques <igorma.academico@gmail.com>
2019-06-14 21:31:02 -0300

    adição de comentários e código COMENTADO que utiliza o tokenizador do spacy

--- segmentador/segmentador_generico_janelas.py
+++ segmentador/segmentador_generico_janelas.py
@@ -5,8 +5,7 @@
 			1.1. Coloca um tag auxiliar para cada token (apenas para fins de formatação), pois é esperado uma lista de tuplas [(p1, t1), ..., (p2, t2)]
 		2. Obtém a lista de features (X)
 		3. Usa o classificador para segmentar o arquivo
-		4. Cria uma pasta com o nome do arquivo
-		5. Escreve cada bloco em um arquivo (usando a tag obtida na classificação no nome do arquivo para identificar o tipo de bloco)
+		4. Salva o arquivo classificado
 
 '''
 
@@ -14,7 +13,8 @@
 import os
 import pickle
 import pycrfsuite
-#import spacy
+#import nltk
+import spacy
 
 
 '''
@@ -25,15 +25,6 @@ def separa_em_blocos(pares_palavra_tag, tamanho_janela = 1):
 	Separa o documento em diferentes blocos/segmentos.
 	Retorna uma lista de documentos/blocos, lista de lista de tuplas.
 	'''
-
-	'''
-	documentos = []
-	documentos.append([pares_palavra_tag[0], pares_palavra_tag[1]]) #primeiro bloco possui apenas a primeira palavra e a segunda
-	for i in range(1, len(pares_palavra_tag) - 1):
-		documentos.append([pares_palavra_tag[i - 1], pares_palavra_tag[i], pares_palavra_tag[i + 1]]) #os blocos centrais possuem a palavra central + as palavras de borda
-	documentos.append([pares_palavra_tag[len(pares_palavra_tag) - 2], pares_palavra_tag[len(pares_palavra_tag) - 1]]) #último bloco possui apenas a última palavra e a panúltima
-	'''
-
 
 	documentos = []
 	for i in range(tamanho_janela):
@@ -109,15 +100,16 @@ def criador_de_features(documento, posicao_do_par, pos_tags_documento):
 	return features
 
 
-def main():
-	'''
-		Abrindo arquivos e formatando
-	'''
-	lista_arquivos = os.listdir('/local/textos_pec6/')
 
-	#nlp = spacy.load('pt_core_news_sm')
+def main():
+	TAMANHO_JANELA = 4
+
+	#lendo arquivos e formatando
+	lista_arquivos = os.listdir('/home/igor/Área de Trabalho/Graduação/Ágora_Digital/github_fork_igor/leggo-content/segmentador/textos_pec6/')[:1]
+
 	pares_palavra_tag = []
 	contador_blocos_documento = []
+	nlp = spacy.load("pt_core_news_sm")
 	for arquivo in lista_arquivos:
 		#print(arquivo)
 		documento_atual = []
@@ -127,22 +119,17 @@ def main():
 			#for token in doc:
 			#	print(token.text, token.pos_, token.dep_)
 			texto = texto.split()
+			#doc = nlp(texto)
+			#texto = [token.text for token in doc]
 			for palavra in texto:
 				documento_atual.append((palavra, 'tag_auxiliar'))
 		contador_blocos_documento.append(len(documento_atual))
 		pares_palavra_tag += documento_atual
-	documentos = separa_em_blocos(pares_palavra_tag, 4)
-	#for doc in documentos[:5]:
-		#print(doc)
-	#print(len(documentos))
-	#exit(0)
+	documentos = separa_em_blocos(pares_palavra_tag, TAMANHO_JANELA)
 
-	'''
-		Obtendo as features
-	'''
 
+	#obtendo features
 	tagger = pickle.load(open("tagger_portugues.pkl", "rb"))
-
 
 	X = [] #lista de lista, cada lista contém as features de palavras de um mesmo documento
 	for documento in documentos:
@@ -152,48 +139,42 @@ def main():
 			Xi.append(criador_de_features(documento, i, pos_tags_documento))
 		X.append(Xi)
 
-
-
-	'''
-		Classificando
-	'''
-
+	
+	#classificando
 	tagger = pycrfsuite.Tagger()
 	tagger.open('modelo.model')
 
 
-	#tenho milhares de blocos
+	#há milhares de blocos
 	#cada arquivo tem um número de blocos salvo em contador_blocos_documento
-	#então pra cada valor nessa lista de tamanhos
+	#então, pra cada valor nessa lista de tamanhos
 		#itera, lendo os N próximos elementos de X (sendo N o tamanho dos blocos)
 			#pega a classificação, adiciona na string de saída
 		#salva a saída no arquivo de predição daquela emenda
 		#continua pro próximo arquivo
 
-
+	
 	iterador_X_global = 0
 	contador_arquivos = 0
 	for num_blocos_atual in contador_blocos_documento:
 		saida_arq = ""
 		for i in range(num_blocos_atual):
 			unidade_x_teste = X[iterador_X_global]
-			#e aqui a avaliação do que vai ser feito depende da localização do bloco. No caso, o tamanho da janela é 4 SEMPRE.
+			#e aqui a avaliação do que vai ser feito depende da localização do bloco. No caso, o tamanho da janela é 4 SEMPRE (ou o valor definido em TAMANHO_JANELA).
 			#assim sendo, os 4 primeiros são casos de borda. os demais são simples de serem tratados
-			
 			
 			tag_pred = tagger.tag(unidade_x_teste) #taggea todo o bloco, mas queremos apenas a palavra que nos interessa e isso depende se ela é caso de borda ou não
 			print(tag_pred)
-			aux = 4 if (i >= 4) else i
+			aux = TAMANHO_JANELA if (i >= TAMANHO_JANELA) else i
 			print(unidade_x_teste[aux][1].split('=')[1] + ' ' + str(aux))
-			if(i >= 4): #se for um bloco não de borda
-				saida_arq += unidade_x_teste[4][1].split('=')[1] + ' ' + tag_pred[4] + '\n' #a tag que nos interessa é a da palavra central, na quarta posição
+			if(i >= TAMANHO_JANELA): #se for um bloco não de borda
+				saida_arq += unidade_x_teste[TAMANHO_JANELA][1].split('=')[1] + ' ' + tag_pred[TAMANHO_JANELA] + '\n' #a tag que nos interessa é a da palavra central, na quarta posição
 			else: #se for um bloco de borda de início
 				saida_arq += unidade_x_teste[i][1].split('=')[1] + ' ' + tag_pred[i] + '\n' #a tag que nos interessa é indexada pelo contador i se ela for borda de início
 
 			print(num_blocos_atual)
 			print(iterador_X_global)
 			print(len(X))
-
 
 			iterador_X_global += 1
 
